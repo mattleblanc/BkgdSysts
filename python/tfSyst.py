0467e09798dbc1e8a4c515c52bf5238dc9d01375
mleblanc <matt.leblanc@cern.ch>
2016-07-13 14:30:38 +0200

    Adding options for defining output more precisely, also output verbosity.

--- python/tfSyst.py
+++ python/tfSyst.py
@@ -17,6 +17,9 @@ parser.add_option("--cutstrings", help="JSON file containing regions and cutstri
 parser.add_option("--systs", help="JSON file which contains the systematics to compute.",
                   default="${ROOTCOREBIN}/../BkgdSysts/config/multib_ichep2k16_ttbar.json")
 parser.add_option("--input", help="Directory of input ROOT files", default="${ROOTCOREBIN}/../BkgdSysts/input/")
+parser.add_option("--output", help="Directory of output files", default="${ROOTCOREBIN}/../BkgdSysts/output/")
+parser.add_option("--systfile", help="Name of output .root file with systematics", default="bkgsyst.root")
+parser.add_option("--verbose",action='store_true', help="Verbose output?", dest="verbose")
 
 (options, args) = parser.parse_args()
 
@@ -45,19 +48,19 @@ for fname in glob.glob(options.input):
             trees[did]=files[did].Get('nominal')
         
 for did in trees:
-    logging.info("Tree for DID\t%s\thas\t%s\tentries.",did,trees[did].GetEntries())
+    if(options.verbose): logging.info("Tree for DID\t%s\thas\t%s\tentries.",did,trees[did].GetEntries())
 
 # Loop over the different regions.
 
-outfile = ROOT.TFile("output/bkgsyst.root","recreate")
+outfile = ROOT.TFile(options.output+options.systfile,"recreate")
 
 for (syst,sets) in systematics.items():
-    logging.info("syst\t\t%s",syst)
-    hist = ROOT.TH1F(syst,syst,50,0,49)
+    logging.info("syst\t%s",syst)
+    hist = ROOT.TH1F(syst,syst,28,0,27)
 
     for region in cutstrings:
         logging.info("region\t\t%s",region)
-    
+
         tfs = {}
         for (scheme,samples) in sets.items():
             yields = {}
@@ -70,10 +73,12 @@ for (syst,sets) in systematics.items():
                     regtype="SR"
                 if string.find(subreg,"CR")>=0 :
                     regtype="CR"
-                if string.find(subreg,"VR")>=0 :
-                    regtype="VR"
+                if string.find(subreg,"VR1")>=0 :
+                    regtype="VR1"
+                if string.find(subreg,"VR2")>=0 :
+                    regtype="VR2"
                         
-                logging.info("Region Type\t\t\t%s",regtype)
+                if(options.verbose): logging.info("Region Type\t\t\t%s",regtype)
                         
                 # So, we want to know the yield for each of those regions, in each sample:
                 nEvents=0.0
@@ -82,7 +87,7 @@ for (syst,sets) in systematics.items():
                         nEvents += trees[did].GetEntries(cuts)
                         yields[regtype] = nEvents
                         
-                logging.info("DID\t\t\t\t\t%s\t%s",scheme,nEvents)
+                if(options.verbose): logging.info("DID\t\t\t\t\t%s\t%s",scheme,nEvents)
 
             for (reg,nEvents) in yields.items():
                 if(reg != "CR" and yields["CR"]!=0):
@@ -92,11 +97,11 @@ for (syst,sets) in systematics.items():
                     tfs[scheme] = -1
                     #hist.Fill(syst+"_"+region+"_"+subreg+"_"+scheme,-1)
 
-        #for (scheme,tf) in tfs.items():
-        if "varied" in tfs:
-            hist.Fill(region,tfs["varied"]/tfs["nominal"])
-        elif "varyUp" and "varyDown" in tfs:   
-            hist.Fill(region,tfs["varyUp"]/tfs["varyDown"])
+            for (reg,nEvents) in yields.items():
+                if "varied" in tfs:
+                    hist.Fill(region+"_"+reg,tfs["varied"]/tfs["nominal"])
+                elif "varyUp" and "varyDown" in tfs:   
+                    hist.Fill(region+"_"+reg,tfs["varyUp"]/tfs["varyDown"])
 
     hist.SetStats(0)
     hist.GetXaxis().SetTitle("Region")
