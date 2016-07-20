9199d29c00c607712af66c546d82408f6ff4f882
Matt LeBlanc <matt.leblanc@cern.ch>
2016-07-20 04:58:38 -0700

    Adding pilot scripts

---
+++ run_singletop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python python/tfSyst.py \
+    --cutstrings cutstrings/multib_ichep2k16_regions_2b.json \
+    --systs config/multib_ichep2k16_singletop.json \
+    --lumi_weights 'config/weights.json' \
+    --event_weights 'weight_mc*weight_jvt*weight_muon*weight_elec*weight_btag' \
+    --input '/global/mleblanc/multib_ichep16/bgsyst/merge/*.root' \
+    --output 'output_singletop/' \
+    --systfile 'bkgsyst_multib_singletop.root' \
+    --atlas \
+    --verbose
---
+++ run_ttbar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python python/tfSyst.py \
+    --cutstrings cutstrings/multib_ichep2k16_regions_splitflav.json \
+    --systs config/multib_ichep2k16_ttbar.json \
+    --lumi_weights 'config/weights.json' \
+    --event_weights 'weight_mc*weight_jvt*weight_muon*weight_elec*weight_btag*weight_WZ_2_2' \
+    --input '/global/mleblanc/multib_ichep16/bgsyst/merge/*.root' \
+    --output 'output_ttbar/' \
+    --systfile 'bkgsyst_multib_ttbar.root' \
+    --atlas \
+    --verbose
