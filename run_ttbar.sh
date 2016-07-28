4e11d03da2ff2e03ea46068b72ccabc3af0f3c0a
mattleblanc <matt.leblanc@cern.ch>
2016-07-28 17:00:49 -0500

    updating for latest running

--- run_ttbar.sh
+++ run_ttbar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python python/tfSyst.py \
-    --cutstrings cutstrings/multib_ichep2k16_regions_splitflav.json \
+    --cutstrings cutstrings/multib_ichep2k16_regions_splitflav_1b.json \
     --systs config/multib_ichep2k16_ttbar.json \
     --lumi_weights 'config/weights.json' \
     --event_weights 'weight_mc*weight_jvt*weight_muon*weight_elec*weight_btag*weight_WZ_2_2' \
-    --input '/global/mleblanc/multib_ichep16/bgsyst/merge/*.root' \
+    --input '/faxbox2/user/mleblanc/multib_ichep2k16/theory/theoryttbar.*.root' \
     --output 'output_ttbar/' \
     --systfile 'bkgsyst_multib_ttbar.root' \
     --atlas \
